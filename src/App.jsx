94c4e15e0561bf64fa839691fc533362dd058f15
Victor okpaire <vixtorjason@gmail.com>
2025-04-24 21:01:19 +0100

    sorted out stying

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
     return (
         <main>
-            <h1 className='text-3xl text-orange-500 font-bold font-[general]'>Welccome to Awwards</h1>
+            <h1 className='text-3xl text-violet-300 font-bold font-circular-web'>Welccome to Awwards</h1>
         </main>
     )
 }
 
-export default App
+export default App;
