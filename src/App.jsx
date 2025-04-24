1ab01c63922ba790f64794e7488e653ce6ce07a6
Victor okpaire <vixtorjason@gmail.com>
2025-04-24 20:12:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className='text-3xl text-orange-500 font-bold font-[general]'>Welccome to Awwards</h1>
+        </main>
+    )
+}
+
+export default App
